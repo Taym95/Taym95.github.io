51e13b5903c95e24c7a294f84821cbadbaddc4c0
Taym95 <haddadi.taym@gmail.com>
2016-12-06 21:02:56 +0100

    Initial commit

---
+++ js/my_script.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+});
+
+
+//var options = [
+//    {selector: '.class', offset: 200, callback: 'globalFunction()'},
+//    {selector: '.other-class', offset: 200, callback: 'globalFunction()'},
+//];
+//Materialize.scrollFire(options);
+//
+//
+//var options = [
+//    {selector: '#staggered-test', offset: 50, callback: 'Materialize.toast("This is our ScrollFire Demo!", 1500 )'},
+//    {selector: '#staggered-test', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )'},
+//    {selector: '#staggered-test', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")'},
+//    {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")'}
+//];
+//Materialize.scrollFire(options);
+//
+//
+//
+//Materialize.showStaggeredList('#staggered-test')
